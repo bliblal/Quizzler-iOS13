1e4472e5450794769355c319665f143896c022bf
Bilal <javaid.billal@gmail.com>
2024-01-05 10:59:32 +0500

    add structs

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Questions.swift
+//  Quizzler-iOS13
+//
+//  Created by Bilal Javed on 04/01/2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Question{
+    var q:String
+    var a:String
+    
+    init(q: String, a: String) {
+        self.q = q
+        self.a = a
+    }
+}
